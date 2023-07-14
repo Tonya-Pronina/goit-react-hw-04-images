4fc7fb225f36d61bb15b8230ee856cdc7e34d9f0
Tonya-Pronina <darkpixus@gmail.com>
2023-07-15 01:39:25 +0300

    +

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+// import React from 'react';
+// import css from 'components/Modal/Modal.module.css';
+// import PropTypes from 'prop-types';
+// import { useEffect } from 'react';
+
+// export const Modal = ({ onModalClose, largeImageURL }) => {
+//   const handleKeyDown = e => {
+//     if (e.keyCode === 27 || e.currentTarget === e.target) {
+//       onModalClose();
+//     }
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener('keydown', handleKeyDown);
+//     document.body.style.overflow = 'hidden';
+
+//     return () => {
+//       window.removeEventListener('keydown', handleKeyDown);
+//       document.body.style.overflow = 'auto';
+//     };
+//   }, [onModalClose, handleKeyDown]);
+
+//   return (
+//     <div className={css.backdrop} onClick={handleKeyDown}>
+//       <div className={css.modal}>
+//         <img src={largeImageURL} alt="" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// Modal.propTypes = {
+//   largeImageURL: PropTypes.string.isRequired,
+//   onModalClose: PropTypes.func.isRequired,
+// };
+
+import React, { useEffect, useCallback } from 'react';
 import css from 'components/Modal/Modal.module.css';
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
 
 export const Modal = ({ onModalClose, largeImageURL }) => {
-  const handleKeyDown = e => {
-    if (e.keyCode === 27 || e.currentTarget === e.target) {
-      onModalClose();
-    }
-  };
+  const handleKeyDown = useCallback(
+    e => {
+      if (e.keyCode === 27 || e.currentTarget === e.target) {
+        onModalClose();
+      }
+    },
+    [onModalClose]
+  );
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
@@ -18,7 +56,7 @@ export const Modal = ({ onModalClose, largeImageURL }) => {
       window.removeEventListener('keydown', handleKeyDown);
       document.body.style.overflow = 'auto';
     };
-  }, [onModalClose, handleKeyDown]);
+  }, [handleKeyDown]);
 
   return (
     <div className={css.backdrop} onClick={handleKeyDown}>
